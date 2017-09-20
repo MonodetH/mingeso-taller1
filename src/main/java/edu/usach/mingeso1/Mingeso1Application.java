0a263c6e207a16df4d77b8f27f2e056b6a80c680
Javier <javierkns@gmail.com>
2017-09-19 23:00:45 -0300

    spring.io initializer

---
+++ src/main/java/edu/usach/mingeso1/Mingeso1Application.java
@@ -0,0 +1,12 @@
+package edu.usach.mingeso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mingeso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mingeso1Application.class, args);
+	}
+}
